deed608a7bf939657f6c63abcf08d7595e9f21e8
saiteja-tally <saiteja.g@tallysolutions.com>
2023-11-27 00:44:50 +0530

    removed comments

--- python/1376.py
+++ python/1376.py
@@ -1,12 +1,5 @@
 class Solution(object):
     def numOfMinutes(self, n, headID, manager, informTime):
-        """
-        :type n: int
-        :type headID: int
-        :type manager: List[int]
-        :type informTime: List[int]
-        :rtype: int
-        """
         adj = {}
 
         for i in range(len(manager)):
